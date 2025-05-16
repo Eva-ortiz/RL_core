c71a2489aa04d8bd2c909f5bf5e39639015863a9
Eva-ortiz <evaorma2@gmail.com>
2025-05-16 19:07:24 +0200

    refactor : approximated_simulation generic pipeline more similar to maskedPPO_agent

--- RLcore/simulations.py
+++ RLcore/simulations.py
@@ -1,4 +1,3 @@
-import copy
 import logging
 import sys
 import time
@@ -6,15 +5,13 @@ from pathlib import Path
 from typing import Literal
 
 import gymnasium as gym
-import numpy as np
 import torch
 from agent_predict import maskablePPO_episode
 from agent_train import maskablePPO_train
 from global_vars import WORKING_DIR
+from RL.DRL import DQN_agent, DRL_agent  # noqa E402
 from sb3_contrib import MaskablePPO
-
-from RLcore.RL.DRL import DQN_agent, DRL_agent  # noqa E402
-from RLcore.RL.utils import exists, load_conf, set_logging, store_series_to_hdf, timer
+from utils import load_conf, set_logging, timer
 
 # for HPC, specify the path to import from our modules
 # .. [1] https://stackoverflow.com/questions/4383571/importing-files-from-different-folder
@@ -113,14 +110,14 @@ def maskedPPO_agent(
 
 def approximated_simulation(
     env: gym.Env,
+    train: bool,
+    greedy_eval: bool,
     algorithm: Literal["Sarsa", "Q-learning", "double_Q-learning"],
-    cfg_path: Path = Path("./RLcore/config.toml"),
-    save_computed_htc: bool = False,
-    save_htc_csv: bool = True,
-    save_q_net: bool = True,
-    plot: bool = False,
     logging_level: str = "warn",
+    model_path: Path = Path("./models/"),
+    conf_path: str = "./config.toml",
     seed: int | None = None,
+    **env_kwargs,
 ):
     """RL approximated method simulation of the agent in environment.
 
@@ -130,36 +127,28 @@ def approximated_simulation(
     ----------
     env : gym.Env
         Environment class to run the input agent.
+    train: bool
+        Train the agent or load it from `model_path`.
+    greedy_eval: bool
+        Perform a greedy episode for agent evaluation and save its info.
     algorithm : Literal["Sarsa", "Q-learning", "double_Q-learning"]
         Determine the RL algorithm to use. Integrated algorithms are:
             * Sarsa
             * Q-learning
             * double Q-learning
-    cfg_path : Path, optional
-        Configuration file path. By default, Path("./config.toml").
-    save_computed_htc : bool, optional
-        Overwrite stored htc values in order to add new computed values. If
-        several runs are executed at the same time, this could lead to execution
-        failure.
-        By default, False.
-    save_htc_csv : bool, optional
-        Select to store a csv with the htc value of each state. Useful to
-        visualize those values. By default, True.
-    save_q_net : bool, optional
-        Select to save the action-state values network.
-        By default, True.
-    plot : bool, optional
-        Select if we desire to obtain different plots, such as:
-         - Loss curves
-         - Reward curves
-        By default, False.
     logging_level : str, optional
         Select logging level. Additionally, if `debug`/`info` are selected, set
-        verbose to True, else, to False.
-        By default, `warn`.
+        verbose to True, else, to False. By default, `warn`.
+    model_path : Path, optional
+        Parent path where agents and their evaluation info are saved.
+        By default, Path("./models/").
+    conf_path : str, optional
+        Configuration file path. By default, Path("./config.toml").
     seed : int | None, optional
         Seed number along the simulation. If None, do not fix any seed.
         By default, None.
+    **env_kwargs
+        Environment inputs.
 
     Returns
     -------
@@ -172,18 +161,6 @@ def approximated_simulation(
         We will assume optimal state is the state where the agent tends to end.
         Because of that, we will perform a very long simulation with a greedy
         policy and output the end state as the OPTIMAL STATE.
-
-    Warnings
-    --------
-    `HTC_{n_layers}_layers.h5` input can be problematic.
-        If it has been previously **UNsuccessfully** generated, a HDF5ExtError
-        will raise.
-        To solve it, delete `HTC_{n_layers}_layers.h5` and execute twice
-        the simulation, as the first modification to the file will raise a
-        ValueError as file is already opened and it is required to be in
-        read-only mode.
-        Another solution is to delete the problematic file and copy-paste it
-        from HTC_backup.
 
     References
     ----------
@@ -198,15 +175,11 @@ def approximated_simulation(
     verbose = logging_level in ["debug", "info"]
 
     # load the configuration
-    cfg_logging = load_conf(cfg_path, key="logging")
-
-    cfg = load_conf(cfg_path, key="RHT")
-    cfg_plots = cfg["plots"]
-
-    cfg = load_conf(cfg_path, key="DRL")
-    cfg_algorithm = cfg[algorithm]
+    cfg = load_conf(conf_path)
+    cfg_logging = cfg["logging"]
+    cfg_drl = cfg["DRL"]
+    cfg_algorithm = cfg_drl[algorithm]
     cfg_hiperpar = cfg_algorithm["RHT"]["hiperparams"]
-    cfg_save_folder = cfg_algorithm["RHT"]["generic_save_folder"]
 
     # We want to be able to train our model on a hardware accelerator like the
     # GPU or MPS, if available.
@@ -219,107 +192,102 @@ def approximated_simulation(
     )
     logging.info(f"Using {device} device")
 
-    # store start time of the program
-    start_time = time.time()
-
-    # load HTC input constants
-    # frequencies range
-    with open("./data/HTC/HTC_frequencies.npy", "rb") as f:
-        freq_range = np.load(f)
-    # materials permitivities at each frequencies
-    with open("./data/HTC/HTC_permitivities.npy", "rb") as f:
-        mat_per = np.load(f)
-
-    # create the RHT environment
-    env = env(mat_per, freq_range, cfg_path=cfg_path, seed=seed)
-
-    # create the new agent with selected algorithm
+    # select RL algorithm
     agent_class = DQN_agent if algorithm == "double_Q-learning" else DRL_agent
 
-    agent = agent_class(
-        algorithm=algorithm,
-        actions=env.action_space,
-        save_folder=cfg_save_folder,
-        verbose=verbose,
-        hidden_layers=cfg_hiperpar["hidden_layers"],
-        hidden_neur=cfg_hiperpar["hidden_neur"],
-    )
+    if train:
+        # store start time of the program
+        start_time = time.time()
 
-    agent.train(
-        device=device,
-        environment=env,
-        discount_rate=cfg_hiperpar["discount_rate"],
-        lr=cfg_hiperpar["lr"],
-        reduce_perc_lr=cfg_hiperpar["reduce_perc_lr"],
-        min_lr=cfg_algorithm["min_lr"],
-        epsilon=cfg_hiperpar["epsilon"],
-        reduce_eps=cfg_hiperpar["reduce_eps"],
-        min_eps=cfg_algorithm["min_eps"],
-        batch_size=cfg_hiperpar["batch_size"],
-        max_steps=cfg_hiperpar["max_steps"],
-        tol_loss=cfg_hiperpar["tol_loss"],
-        plot_learning_curves=plot,
-        save_q_net=save_q_net,
-        memory_size=cfg_hiperpar.get("memory_size", None),  # only DQN
-        n_batch_per_step=cfg_hiperpar.get("n_batch_per_step", None),  # only DQN
-        n_new_experiences_per_step=cfg_hiperpar.get(
-            "n_new_experiences_per_step", None
-        ),  # only DQN
-        target_estimation_mode=cfg_hiperpar.get(
-            "target_estimation_mode", None
-        ),  # only DQN
-        n_steps_for_target_net_update=cfg_hiperpar.get(
-            "n_steps_for_target_net_update", None
-        ),  # only DQN
-        episode_start_state=cfg_hiperpar["episode_start_state"],
-        decorrelated=cfg_hiperpar["decorrelated"],
-        step_count=cfg_hiperpar["step_count"],
-        reward_curve_mode=cfg_plots["reward_curve_mode"],
-        reward_curve_steps_per_point=cfg_plots["reward_curve_steps_per_point"],
-        debug_counter=cfg_logging["debug_counter"],
-        seed=seed,
-    )
+        # create the RHT environment
+        train_env = env(**env_kwargs)
 
-    # print execution time of this problem
-    timer(start_time, time.time())
+        # create the new agent with selected algorithm
+        agent = agent_class(
+            algorithm=algorithm,
+            actions=train_env.action_space,
+            save_folder=model_path,
+            verbose=verbose,
+            hidden_layers=cfg_hiperpar["hidden_layers"],
+            hidden_neur=cfg_hiperpar["hidden_neur"],
+        )
+
+        agent.train(
+            device=device,
+            environment=train_env,
+            discount_rate=cfg_hiperpar["discount_rate"],
+            lr=cfg_hiperpar["lr"],
+            reduce_perc_lr=cfg_hiperpar["reduce_perc_lr"],
+            min_lr=cfg_algorithm["min_lr"],
+            epsilon=cfg_hiperpar["epsilon"],
+            reduce_eps=cfg_hiperpar["reduce_eps"],
+            min_eps=cfg_algorithm["min_eps"],
+            batch_size=cfg_hiperpar["batch_size"],
+            max_steps=cfg_hiperpar["max_steps"],
+            tol_loss=cfg_hiperpar["tol_loss"],
+            plot_learning_curves=True,
+            save_q_net=True,
+            memory_size=cfg_hiperpar.get("memory_size", None),  # only DQN
+            n_batch_per_step=cfg_hiperpar.get("n_batch_per_step", None),  # only DQN
+            n_new_experiences_per_step=cfg_hiperpar.get(
+                "n_new_experiences_per_step", None
+            ),  # only DQN
+            target_estimation_mode=cfg_hiperpar.get(
+                "target_estimation_mode", None
+            ),  # only DQN
+            n_steps_for_target_net_update=cfg_hiperpar.get(
+                "n_steps_for_target_net_update", None
+            ),  # only DQN
+            episode_start_state=cfg_hiperpar["episode_start_state"],
+            decorrelated=cfg_hiperpar["decorrelated"],
+            step_count=cfg_hiperpar["step_count"],
+            reward_curve_mode=cfg["RHT"]["plots"]["reward_curve_mode"],
+            reward_curve_steps_per_point=cfg["RHT"]["plots"][
+                "reward_curve_steps_per_point"
+            ],
+            debug_counter=cfg_logging["debug_counter"],
+            seed=seed,
+        )
+
+        # print execution time of this problem
+        timer(start_time, time.time())
 
     # ------------- output relevant data -------------
-    _, last_reward, _, last_state, last_action = agent.greedy_simulation(
-        q_net=agent.q_net,
-        environment=copy.deepcopy(env),  # [3]
-        start_state=cfg_hiperpar["episode_start_state"],
-        steps=env.n_layers * 10,
-        device=device,
-    )
-    print(
-        f"Last state is: {last_state}",
-        f"\nWith an associated reward of: {f'{last_reward:.3f}'}",
-    )
-    logging.info(f"Associated last action was {last_action}")
-    logging.info(
-        f"Number of visited states: {len(agent.visited_states)}/{len(env.state_space)}"
-        if exists(env.state_space)
-        else ""
-    )
-    logging.info(
-        f"Number of reached states: {len(agent.max_htc_vs_known_state.iterations)}"
-        f"/{len(env.state_space)}"
-        if exists(env.state_space)
-        else ""
-    )
+    if greedy_eval:
+        eval_env = env(**env_kwargs)
 
-    # store the obtained states htc into hdf and csv (if selected)
-    if save_computed_htc:
-        store_series_to_hdf(
-            f"{env.htc_path}/HTC_{env.n_layers}_layers.h5", env.htc_values
+        # create the new agent with selected algorithm
+        agent = agent_class(
+            algorithm=algorithm,
+            actions=eval_env.action_space,
+            save_folder=cfg_algorithm["RHT"]["generic_save_folder"],
+            verbose=verbose,
+            hidden_layers=cfg_hiperpar["hidden_layers"],
+            hidden_neur=cfg_hiperpar["hidden_neur"],
         )
 
-    if save_htc_csv:
-        env.htc_values.to_csv(
-            f"./data/{cfg_save_folder}/state_{env.n_layers}_layers_htc.csv"
+        agent.greedy_simulation(
+            q_net=agent.q_net,
+            environment=eval_env,  # [3]
+            start_state=cfg_hiperpar["episode_start_state"],
+            steps=eval_env.n_layers * 10,
+            device=device,
         )
 
-    return agent, env, last_state
+        logging.info(
+            f"""Number of visited states:
+             {len(agent.visited_states)}/{len(eval_env.state_space)}"""
+            if eval_env.state_space is not None
+            else ""
+        )
+        logging.info(
+            f"Number of reached states: {len(agent.max_htc_vs_known_state.iterations)}"
+            f"/{len(eval_env.state_space)}"
+            if eval_env.state_space is not None
+            else ""
+        )
+
+    print("DEEP REINFORCEMENT LEARNING IS DONE!")
 
 
 if __name__ == "__main__":
