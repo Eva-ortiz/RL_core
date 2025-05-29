e5d98adc1e73a799645f1f1c371ad330dbbdbd61
Eva-ortiz <evaorma2@gmail.com>
2025-05-29 15:35:00 +0200

    docs : speceify step input is action index

--- RLcore/environment.py
+++ RLcore/environment.py
@@ -222,7 +222,7 @@ class ENV(gym.Env):  # type: ignore[type-arg]
         Parameters
         ----------
         action : Action
-            [MANDATORY INPUT] Input action.
+            [MANDATORY INPUT] Input action index.
             Correspondence between idx representation of the action and action
             name is given by `_action_name_dict` attribute.
 
