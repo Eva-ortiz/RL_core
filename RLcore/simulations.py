3ad11273555a3110c3ce034e479be1753447b368
Eva-ortiz <evaorma2@gmail.com>
2025-06-03 13:33:01 +0200

    fix : use defined agent and environment attributes

--- RLcore/simulations.py
+++ RLcore/simulations.py
@@ -253,7 +253,7 @@ def approximated_simulation(
 
         logging.info(
             f"""Number of visited states:
-             {len(agent.visited_states)}/{eval_env.shape[0]}"""
+             {len(agent.visited_states_norm)}/{eval_env.observation_space.shape[0]}"""
         )
 
     print("DEEP REINFORCEMENT LEARNING IS DONE!")
