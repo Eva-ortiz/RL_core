e6ef9c946ac3e6f94f0c22862b75f804787604e6
Eva-ortiz <evaorma2@gmail.com>
2025-06-24 23:43:37 +0200

    fix : remove undesired tuple

--- RLcore/RL/DRL.py
+++ RLcore/RL/DRL.py
@@ -615,7 +615,7 @@ class DRL_agent(agent):
         if epsilon > rand:
             # change related to invalid action masking
             valid_actions = (
-                np.where(action_masks)
+                np.where(action_masks)[0].astype(Action)
                 if action_masks is not None
                 else range(len(self.actions) - 1)
             )
