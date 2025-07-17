2b1299851a3f17947f817713debe475ed7d7faa4
Eva-ortiz <evaorma2@gmail.com>
2025-07-17 17:31:43 +0200

    feat : add masking option in train method

--- RLcore/RL/DRL.py
+++ RLcore/RL/DRL.py
@@ -754,6 +754,7 @@ class DRL_agent(agent):
         batch_size: int = 64,
         max_steps: int = np.inf,
         tol_loss: float = 0.0,
+        use_masking: bool = False,
         plot_learning_curves: bool = True,
         save_q_net: bool = True,
         **kwargs,
@@ -782,6 +783,9 @@ class DRL_agent(agent):
             By default, np.inf
         tol_loss : float, optional
             Tolerance to consider action values have converged. By default, 0.0
+        use_masking : bool, optional
+            Whether or not to use invalid action masks during training, by
+            default False.
         plot_learning_curves : bool, optional
             Select to plot learning curves or not. By default, True
         save_q_net : bool, optional
@@ -989,6 +993,7 @@ class DRL_agent(agent):
                 initial_batch_action,
                 follow_next_action,
                 decorrelated,
+                use_masking=use_masking,
                 reset_options=reset_options,
                 episode_length=episode_length,
             )
@@ -1097,6 +1102,7 @@ class DRL_agent(agent):
                     epsilon,
                     training_best_reward,
                     reset_options,
+                    use_masking,
                 )
 
             # reduction of epsilon at each episode, with a min value of min_eps
