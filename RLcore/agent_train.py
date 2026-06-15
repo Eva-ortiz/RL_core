840c2f6ca2e6644c1d3a36f908b72b20e2ff9222
Eva <68061770+Eva-ortiz@users.noreply.github.com>
2026-06-15 21:35:08 +0200

    feat: net_arch from config (#25)

--- RLcore/agent_train.py
+++ RLcore/agent_train.py
@@ -371,6 +371,7 @@ def maskablePPO_train(
     .. [8] https://optuna.readthedocs.io/en/stable/faq.html#how-can-i-obtain-reproducible-optimization-results
     .. [9] https://optuna.readthedocs.io/en/stable/faq.html#how-are-exceptions-from-trials-handled
     .. [10] https://stable-baselines3.readthedocs.io/en/master/guide/vec_envs.html#vecenv-api-vs-gym-api
+    .. [11] https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html#custom-networks
     .. [12] https://stable-baselines3.readthedocs.io/en/master/common/utils.html#stable_baselines3.common.utils.get_linear_fn
     """
     ppo_cfg = load_conf(cfg_path)["agent"]["PPO"]
@@ -479,6 +480,7 @@ def maskablePPO_train(
                 policy="MlpPolicy",
                 env=agent_env,
                 verbose=0,
+                policy_kwargs={"net_arch": ppo_cfg.get("net_arch")},  # [11]
                 seed=ppo_cfg["seed"],
                 _init_setup_model=True,
                 **trial_params,
@@ -572,6 +574,7 @@ def maskablePPO_train(
             policy="MlpPolicy",
             env=monitored_env,
             verbose=2 if verbose else 1,
+            policy_kwargs={"net_arch": ppo_cfg.get("net_arch")},  # [11]
             seed=ppo_cfg["seed"],
             _init_setup_model=True,
             **train_params,
