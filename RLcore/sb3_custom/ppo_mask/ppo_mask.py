bfe59a9246c3c4a6c4d44b240f12d8dab0fdf3b2
Eva <68061770+Eva-ortiz@users.noreply.github.com>
2026-06-15 21:18:38 +0200

    fix: initialize action_mask (#23)

--- RLcore/sb3_custom/ppo_mask/ppo_mask.py
+++ RLcore/sb3_custom/ppo_mask/ppo_mask.py
@@ -53,6 +53,7 @@ class MaskablePPO_custom(MaskablePPO):
 
         terminated, truncated = False, False
         rewards = []
+        action_masks = None
         while not terminated and not truncated:
             # This is the only change related to invalid action masking
             # (comment from sb3_contrib.MaskablePPO)
